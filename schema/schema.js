bc451466d359fac8db3eef8645a334f0839dd35d
Iggyfufu <kennethzhu07@gmail.com>
2018-09-27 16:28:48 -0400

    Feat(GraphQL Schema): Added Schema: UserType, RootQuery, and imported Schema to express route

--- schema/schema.js
+++ schema/schema.js
@@ -1,10 +1,19 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
 const {
   GraphQLObjectType,
   GraphQLString,
   GraphQLInt,
+  GraphQLSchema,
 } = graphql;
+
+
+const users = [
+  { id: '23', firstName: 'Ken', age: 28 },
+  { id: '47', firstName: 'Stella', age: 28 },
+];
+
 
 const UserType = new GraphQLObjectType({
   name: 'User',
@@ -15,3 +24,19 @@ const UserType = new GraphQLObjectType({
   },
 });
 
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: 'UserType',
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return _.find(users, { id: args.id });
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
