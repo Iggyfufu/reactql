04538a35ee21f80e0b3c50f9a7ae2c94e957c0a2
Iggyfufu <kennethzhu07@gmail.com>
2018-09-27 16:34:48 -0400

    Fix(Schema File): RootQuery, fields, user, type was string converted to variable

--- schema/schema.js
+++ schema/schema.js
@@ -28,7 +28,7 @@ const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
     user: {
-      type: 'UserType',
+      type: UserType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
         return _.find(users, { id: args.id });
