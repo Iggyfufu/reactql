0016dcf7d25e631b275076bb1a40ecb1ec8805e1
Iggyfufu <kennethzhu07@gmail.com>
2018-10-11 16:59:21 -0400

    Feat(Create SongList Component) Created SongList Component and imported to root

---
+++ Lyrical-GraphQL/client/Components/SongList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+// import gql from 'graphql-tag'
+
+class SongList extends Component {
+  render() {
+    return (
+      <div />
+    );
+  }
+}
+
+export default SongList;
+
